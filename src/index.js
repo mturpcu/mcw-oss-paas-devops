b25e8b8dec07e39197f7a7fa332aa29ab2be28f3
Ubuntu <murat.turpcu@gmail.com>
2018-10-30 07:14:01 +0000

    function eklendi

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
-registerServiceWorker();
+module.exports = function (context, newOrders) {
+	for(var i=0; i < newOrders.length; i++) {
+		var newOrder = newOrders[i];
+		var orderId = newOrder['$v']['id']['$v'];
+		var userId = newOrder['$v']['userId']['$v'];
+		var sendNotification = newOrder['$v']['sendNotification']['$v'];
+		var processed = newOrder['$v']['processed']['$v'];
+		if (!processed) {
+			context.log('Sending order to orderqueue for order id ', orderId);
+			context.bindings.outputQueue = {
+				orderId: orderId,
+				userId: userId,
+				sendNotification: sendNotification
+			};
+		}
+		else {
+			context.log('Already processed, not sent to orderqueue for order id ', orderId);
+		}
+    }
+	context.done();
+}
